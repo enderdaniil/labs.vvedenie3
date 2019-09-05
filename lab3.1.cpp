1934113c0b3296c023006e9f76af73a35b659dc0
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-05 22:29:06 +0300

    Create lab3.1.cpp

---
+++ lab3.1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int
+main ()
+{
+  cout << "Vvedite chisla" << endl;
+  float a, b, c;
+  cin>>a>>b;
+  c=a;
+  a=b;
+  b=c;
+  cout << "A=" <<a<< ", B=" <<b;
+}
