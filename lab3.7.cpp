bd24dd78b727279b2360e95be18b507cbc2a730f
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-05 22:35:48 +0300

    Create lab3.7.cpp

---
+++ lab3.7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a1,a2,a3;
+    cout<<"Vvedite chislo"<<endl;
+    cin>>a1;
+    a2=a1*a1;
+     a3=a1*a2;
+      a3=a3*a2; //5
+       a1=a3*a3; //10
+        a2=a1*a3;
+    cout<<a2;
+}
