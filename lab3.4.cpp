8e71e25cfc47bbf274ddeea2768c834d3ae8b778
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-05 22:33:44 +0300

    Create lab3.4.cpp

---
+++ lab3.4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"VVedite X"<<endl;
+    cin>>x;
+    cout<<"Y="<<3*pow(x,6)-6*pow(x,2)-7<<endl;
+}
